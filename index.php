62a46b52167d2d28acc8f1b204396fea049b0f83
Unknown <tangetange@ymail.com>
2018-07-13 14:07:30 +0200

    Rio Tangé
    Page internet pour Rio de Janeiro. You know ?

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<!--
+-->
+<html>
+    <head>
+        <title>RIO DE JANEIRO</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <style>
+            
+            *{
+               // border: 1px solid black;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <main class="container">
+            <header class="row"> 
+                <div class="col col-lg-5">
+
+                    <img src="RIOlogo.png" width="100" >
+                </div>
+                
+                <nav class="col col-lg-7">
+                        <img src="">
+                               <a href='#'>ACCEUIL</a>
+                               <a href='#'>ORGANISATION</a>
+                               <a href='#'>SPORTS</a>
+                               <a href='#'> INFRASTRUCTURES</a>
+                          
+                </nav>
+            </header>
+            <section >
+                <div id="plageId" class="plageClass salut bonjour">
+                    <div id="transp">
+                        <span id="gauche">Zoom sur les Jeux Olympiques de Rio 2016</span>
+                        <span id="droite">Voir l'article</span> 
+                    </div>
+                </div>
+                        <div Id="wrapper">
+                            <article>
+                                <h1>PLUS D'INFORMATIONS  SUR  RIO DE JANEIRO</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat, lorem sit amet luctus elementum, lacus ipsum pharetra ante, sed porttitor nisi nunc sit amet diam. Pellentesque nec fermentum eros, ac dapibus dolor. Maecenas eget interdum turpis. Aliquam pellentesque imperdiet tristique. Ut vel odio a ex egestas sodales non id nulla. Nunc ut mattis lorem, non egestas leo. Sed elementum vel erat eget placerat. Ut volutpat massa eros, id pharetra ex ullamcorper vitae. Nam sit amet tempus turpis. Sed placerat dui libero, vitae venenatis enim pulvinar id. In vitae dui non leo auctor sollicitudin. Pellentesque consectetur, purus rhoncus posuere bibendum, neque sapien cursus velit, vel pretium leo arcu a eros. Praesent diam leo, sagittis et gravida vel, viverra at mauris.stibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla tincidunt tellus sed ex auctor pulvinar. Proin non mi ipsum. Ut molestie, elit blandit maximus vehicula, velit nunc fringilla diam, eu ornare sapien lorem vel urna. Integer in risus sed nunc blandit tempus. Morbi quis ligula et risus feugiat tincidunt at id ante. Ut vulputate a massa vitae vestibulum. Cras et cursus felis, sit amet dapibus enim. Nullam ut imperdiet purus. In hac habitasse platea dictumst. Curabitur ullamcorper mi et sem rutrum, gravida porta velit porta. In faucibus metus in tristique posuere. In malesuada, ante non venenatis pulvinar, eros purus tempus nunc, at ultricies nisi tellus a metus.</p>
+                                    <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla tincidunt tellus sed ex auctor pulvinar. Proin non mi ipsum. Ut molestie, elit blandit maximus vehicula, velit nunc fringilla diam, eu ornare sapien lorem vel urna. Integer in risus sed nunc blandit tempus. Morbi quis ligula et risus feugiat tincidunt at id ante. Ut vulputate a massa vitae vestibulum. Cras et cursus felis, sit amet dapibus enim. Nullam ut imperdiet purus. In hac habitasse platea dictumst. Curabitur ullamcorper mi et sem rutrum, gravida porta velit porta. In faucibus metus in tristique posuere. In malesuada, ante non venenatis pulvinar, eros purus tempus nunc, at ultricies nisi tellus a metus.</p>
+                                    <p>In cursus ornare orci vitae dapibus. Suspendisse fringilla nunc quis felis faucibus facilisis. Etiam et mi sem. Integer volutpat hendrerit est vulputate hendrerit. Vestibulum tempus risus eget ex lobortis tempus. Integer consectetur ligula ut sagittis dignissim. Ut sit amet enim a magna ullamcorper fringilla at sed odio. Nulla facilisi. Morbi id dignissim nibh, in commodo enim. Suspendisse malesuada magna id imperdiet lacinia. Praesent ligula nisl, iaculis vitae elementum tempor, convallis vitae nunc. Aenean ornare interdum auctor. Integer luctus sit amet mauris id rutrum. Sed bibendum, est vel blandit scelerisque, erat dolor finibus tortor, et sodales nisi felis non nibh. Duis tincidunt ante in scelerisque mattis. Proin vitae elit vel est pulvinar aliquet.</p>
+                            </article>
+                            <aside>
+                                <h1>À PROPOS DE RIO</h1>
+                                <IMG SRC="">
+                                <span>
+                                    <h2>Administration</h2>
+                                    Pays:	 Brésil
+                                    <br>Région:	Sudeste<br/>
+                                    État:	État de Rio de Janeiro Rio de Janeiro 
+                                    Langue(s):	Portugais
+                                    Maire:	Marcelo Crivella (PRB)
+                                    Code postal:	20000-000
+                                    Fuseau horaire:	UTC-3
+                                    Indicatif:	021
+                                    <h2>Démographie</h2>
+                                    Gentilé:	Carioca, Carioque
+                                    <br>Population:	6 093 472 hab.1 (04/2007)<br/>
+                                    Densité:	4 836 hab./km2
+                                    Population de l'agglomération:	12 620 000 hab.
+                                    <h2>Géographie</h2>
+                                    Coordonnées:	22° 54′ 35″ sud, 43° 10′ 35″ ouest
+                                    Altitude:	380 m
+                                    <br>Superficie:	126 000 ha = 1 260 km2<br/>
+                                    <h2>Divers</h2> 
+                                    Site(s) touristique(s): Christ Rédempteur, Mont du Pain de Sucre, Plages de Copacabana et d'Ipanema, Pedra da Gávea, Carnaval de Rio
+                                    Fondateur: Estácio de Sá
+                                    Date de fondation:
+                                    1er mars 1565
+
+                                </span>
+
+                            </aside>
+                        </div>   
+                </div>
+                
+            </section>
+            <footer >
+                <div><h2>NEWSLETTER</h2></div>
+                <div><h2>PHOTOS</h2></div>
+                <div><h2>PARTENAIRES</h2></div>
+                
+            </footer>
+        </main>
+    </body>
+</html>
+
